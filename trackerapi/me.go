dbfb6ce4aa481699420573d1eb0a3c8f615b0709
Skylar Brown <skylarmb@gmail.com>
2016-11-23 11:59:09 -0800

    find and replace github references

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/skylarmb/clirescue/cmdutil"
+	"github.com/skylarmb/clirescue/user"
 )
 
 var (
